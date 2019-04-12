83c8a66f56f40c7eec19c9adf2abae58a3e135ab
Raunak Lohiya <rklohiya1996@gmail.com>
2019-04-12 18:52:43 +0530

    Added corrections to code

--- reader.py
+++ reader.py
@@ -4,11 +4,13 @@ import os
 import parameters
 
 # Separates index column and stock column
-def read_csv_or_excel_file(filename, index_col='Time', stock_index_column_name=None):
+def read_csv_or_excel_file(filename, index_col):
     if(filename.endswith('xlsx')):
-        df = pd.read_excel(os.path.join(parameters.INPUT_DIRECTORY, filename), index_col=index_col)
+        df = pd.read_excel(os.path.join(parameters.INPUT_DIRECTORY, filename), index_col = index_col)
     else:
-        df = pd.read_csv(os.path.join(parameters.INPUT_DIRECTORY, filename), index_col=index_col)
+        df = pd.read_csv(os.path.join(parameters.INPUT_DIRECTORY, filename), index_col = index_col)
 
-    if(stock_index_column_name != None): df_index = df[stock_index_column_name]
-    return df_index, df
+    if(parameters.STOCK_INDEX_COLUMN_NAME != None):
+        df_index = df[[parameters.STOCK_INDEX_COLUMN_NAME]]
+        return df_index, df.drop(columns = [parameters.STOCK_INDEX_COLUMN_NAME])
+    return pd.DataFrame(), df
