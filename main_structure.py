7d8219d495d26291b61dac1ee740e3d0c3f1e527
Raunak Lohiya <rklohiya1996@gmail.com>
2019-04-12 13:27:15 +0530

    Updated inside structure

--- main_structure.py
+++ main_structure.py
@@ -3,52 +3,46 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns; sns.set()
-plt.style.use('ggplot')
-number_of_days = 240 #for daily data
-number_of_weeks = 52 #for weekly data
-
-#Read data
-datos = pd.read_excel('s&p500.xlsx',index_col='Time')
+import parameters
+import metrics
+#import seaborn as sns; sns.set()
+#plt.style.use('ggplot')
 
 
-#Normalize data
-normalized_series = (datos/datos.iloc[0])
+# Read data
+stock_index = 's&p500'
+index_data = read_data(file_path)
+datos = pd.read_excel('Stock_Data\\' + stock_index + '.xlsx', index_col='Time')
 
 
-#Rolling Window parameters
-month = 20
-insample_period = 6 * 20
-outsample_period = 20
-total_period = insample_period + outsample_period
+# Portfolio Metrics
+insample_performance = []
+outsample_performance = []
 
-
-#Portfolio Metrics
-insample_performance = pd.DataFrame()
-outsample_performance = pd.DataFrame()
-
-'''
-stats['Annualized Returns(%)'] = daily_returns.mean() * number_of_days * 100
-stats['Annualized Volatility(%)'] = daily_returns.std() * np.sqrt(number_of_days) * 100
-stats['Sharpe Ratio'] = stats['Annualized Returns(%)'] / stats['Annualized Volatility(%)']
-print(82 * '-')
-print('Assets Classes Annualized Statistics - full observation period')
-stats.style.bar(color=['red', 'green'], align='zero')
-'''
 
 # Rolling Window
-for i in range(total_period, datos.shape[0], outsample_period):
-
+for i in range(parameters.TOTAL_PERIOD, datos.shape[0], parameters.OUTSAMPLE_PERIOD):
     # Current Period Data
-    current_data = datos[i-total_period:i]
+    current_data = datos.iloc[i-parameters.TOTAL_PERIOD:i]
 
     # Daily Returns
-    daily_returns = np.log(current_data / current_data.shift(1))
-    daily_returns.dropna(inplace=True)
+    daily_returns = np.log(current_data.div(current_data.shift(1)))
+    daily_returns.dropna(inplace=True, how='all')
+    daily_returns.fillna(0, inplace=True)
 
     # Portfolio Creation
-    weights = np.random.rand(datos.shape[1]) #strategy(daily_returns[:insample_period])
+    weights = np.random.rand(datos.shape[1])
 
-    #Portfolio Metrics
-    insample_performance.append(portfolio_performance(daily_returns[:insample_period]))
-    outsample_performance.append(portfolio_performance(daily_returns[insample_period:]))
+    # Portfolio Metrics for every outsample window
+    insample_performance.append(metrics.portfolio_performance(weights, daily_returns.values[:parameters.INSAMPLE_PERIOD,:]))
+    outsample_performance.append(metrics.portfolio_performance(weights, daily_returns.values[parameters.INSAMPLE_PERIOD:, :]))
+
+
+# Average portfolio metrics
+metrics.average_metrics(insample_performance)
+metrics.average_metrics(outsample_performance)
+
+
+#Save metrics as csv
+metrics.save_list_to_csv(insample_performance, metrics.RESULT_DIRECTORY + '\\insample_' + stock_index + '.csv')
+metrics.save_list_to_csv(outsample_performance, metrics.RESULT_DIRECTORY  + '\\outsample_' + stock_index + '.csv')
